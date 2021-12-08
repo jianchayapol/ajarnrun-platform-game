f5056aa76a7f462183fd2c050fbaf09eca014456
Martin <6330080621@student.chula.ac.th>
2021-12-08 16:09:43 +0700

    implement button package with first button class

---
+++ src/button/MainButton.java
@@ -0,0 +1,72 @@
+package button;
+
+import java.io.FileInputStream;
+
+import javafx.event.EventHandler;
+import javafx.scene.Cursor;
+import javafx.scene.control.Button;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Font;
+
+public class MainButton extends Button {
+	
+	private final String FONT_PATH = "src/res/YanoneKaffeesatz-SemiBold.ttf";
+	private final String BUTTON_PRESSED = "res/button/yellow_button01.png";
+	private final String BUTTON_UNPRESSED = "res/button/yellow_button00.png";
+	
+	public MainButton(String text) {
+		setText(text);
+		setButtonFont();
+		setButtonUnpressedStyle();
+	}
+	
+	private void setButtonFont() {
+		try {
+			setFont(Font.loadFont(new FileInputStream(FONT_PATH), 23));
+		} catch (Exception e) {
+			setFont(Font.font("Verdana", 23));
+		}
+	}
+	
+	private void setButtonPressedStyle() {
+		setStyle(BUTTON_PRESSED);
+		setPrefHeight(45);
+		setLayoutY(getLayoutY() + 4);
+	}
+	
+	private void setButtonUnpressedStyle() {
+		setStyle(BUTTON_UNPRESSED);
+		setPrefHeight(45);
+		setLayoutY(getLayoutY() - 4);
+	}
+	
+	public void initializeButtonListener() {
+		
+		setOnMouseEntered(new EventHandler<MouseEvent>() {
+			public void handle(MouseEvent e) {
+				MainButton.this.getParent().setCursor(Cursor.HAND);
+				setEffect(new DropShadow());
+			}
+		});
+		
+		setOnMouseExited(new EventHandler<MouseEvent>() {
+			public void handle(MouseEvent e) {
+				MainButton.this.getParent().setCursor(Cursor.DEFAULT);
+				setEffect(null);
+			}
+		});
+		
+		setOnMousePressed(new EventHandler<MouseEvent>() {
+			public void handle(MouseEvent e) {
+				setButtonPressedStyle();
+			}
+		});
+		
+		setOnMouseReleased(new EventHandler<MouseEvent>() {
+			public void handle(MouseEvent e) {
+				setButtonUnpressedStyle();
+			}
+		});
+	}
+}

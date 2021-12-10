2a4081d080f897e9d689d2b049b8490e999cffc3
Martin <6330080621@student.chula.ac.th>
2021-12-10 18:20:07 +0700

    implementing levels (not finished yet)

--- src/application/logic/GameController.java
+++ src/application/logic/GameController.java
@@ -5,17 +5,23 @@ import java.util.HashMap;
 
 import javafx.animation.AnimationTimer;
 import javafx.scene.Node;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import player.Player;
+import level.Level;
 
 public class GameController {
 	private HashMap<KeyCode, Boolean> keys = new HashMap<KeyCode, Boolean>();
 	private ArrayList<Node> platforms = new ArrayList<Node>();
 	private Player player;
+	private String[] level;
+	private int levelWidth;
 	
 	
 	public GameController() {
-		
+		setLevelWidth();
+		setFirstLevelPlatform();
 	}
 	
 	public static void platformStart() {
@@ -31,4 +37,32 @@ public class GameController {
 	}
 	private static void initializePlatform() {
 	}
+	
+	private void setLevelWidth() {
+		this.levelWidth = Level.LEVEL1[0].length() * 60;
+	}
+	
+	private void setFirstLevelPlatform() {
+		for (int i = 0; i < Level.LEVEL1.length; i++) {
+			String line = Level.LEVEL1[i];
+			for (int j = 0; j < line.length(); j ++) {
+				switch (line.charAt(j)) {
+				case '0':
+					break;
+				case '1':
+					platforms.add(createImageViewForPlatform(j*60, i*60, 60, 60, "/image/platform/green_05.png"));
+					break;
+				}
+			}
+		}
+	}
+	
+	private ImageView createImageViewForPlatform(int posX, int posY, int width, int height,String url) { 
+		ImageView block = new ImageView(new Image(url));
+		block.setFitWidth(width);
+		block.setFitHeight(height);
+		
+		return block;
+	}
 }
+

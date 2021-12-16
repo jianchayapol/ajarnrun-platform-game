6711766a18394ef98c76c23e1db8f21a641983af
Martin <6330080621@student.chula.ac.th>
2021-12-16 21:12:49 +0700

    javadoc HowToPlayBox, GameSubScene, ViewManager(unfinished)

--- src/gui/element/HowToPlayBox.java
+++ src/gui/element/HowToPlayBox.java
@@ -1,34 +1,62 @@
 package gui.element;
 
 import application.utility.DrawStringUtility;
-import gui.button.ImageButton;
-import gui.button.ImageButtonType;
-import javafx.animation.AnimationTimer;
-import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
 import sharedObject.FontLoader;
 import sharedObject.FontType;
 import sharedObject.RenderableHolder;
-
+/**
+ * HowToPlayerBox is a JavaFx StackPane with a specific String to be add to itself in order to show player how to play the game.
+ * This class' object will be add to howToPlay SubScene (GameSubScene class) as a root.
+ * @author Jian
+ *
+ */
 public class HowToPlayBox extends StackPane {
+	/**
+	 * A background rectangle for this class' object.
+	 */
 	private static Rectangle rec;
+	/**
+	 * An AnchorPane that will contain a String to be shown
+	 */
 	private static AnchorPane mainPane;
+	/**
+	 * A StackPane that will contain background and mainPane
+	 */
 	private static StackPane textPane;
+	/**
+	 * A VBox that will contain the header Label and mainPane
+	 * Then this VBox will be added to this class' object
+	 */
 	private static VBox centerBox;
+	/**
+	 * An ImageView that will be added to mainPane
+	 */
 	private static ImageView img = new ImageView(RenderableHolder.spritePlayerJumpBackward);
+	/**
+	 * Width of this class' object
+	 */
 	private static final int WIDTH = 790;
+	/**
+	 * Height of this class' object
+	 */
 	private static final int HEIGHT = 540;
-
+	/**
+	 * Since this class is a class that is made specifically for one purpose, it will not take any parameter.
+	 * This constructor starts with calling setUpBakground method and initialize centerBox.
+	 * Then create the header Label called howToLabel, set this label's font using FontLoader, set text fill to be yellow
+	 * then add this Label to the centerBox.
+	 * Initialize mainPane as an AnchorPane and use DrawStringUtility's method to draw a how to play text onto the pane.
+	 * Call setUpImage method and setUpPane method then add mainPane to the centerBox.
+	 * Lastly this class' object add both background (bg) and centerBox and setPrefSize and Alignment
+	 */
 	public HowToPlayBox() {
 		setUpBackground();
 		centerBox = new VBox(15);
@@ -45,13 +73,22 @@ public class HowToPlayBox extends StackPane {
 		this.setAlignment(Pos.CENTER);
 		
 	}
-
+	/**
+	 * Initialize rec as a Rectangle with Color.rgb(0,34 75) and Opacity(0.9)
+	 */
 	private void setUpBackground() {
 		rec = new Rectangle(WIDTH, HEIGHT);
 		rec.setFill(Color.rgb(0, 34, 75));
 		rec.setOpacity(0.9);
 	}
-	
+	/**
+	 * Add img (Image View) to mainPane.
+	 * Initialize textPane as a StackPane.
+	 * Add rec and mainPane to textPane
+	 * set textPane's alignment to CENTER_LEFT.
+	 * set centerBox's alignment to CENTER.
+	 * set centerBox's max width to 400.
+	 */
 	private void setUpPane() {
 		mainPane.getChildren().add(img);
 		textPane = new StackPane();
@@ -69,7 +106,9 @@ public class HowToPlayBox extends StackPane {
 	}
 
 	// =====================  instructions Text ===============================
-	
+	/**
+	 * A String that will be shown in howToPlayer SubScene
+	 */
 	public static final String[] instructions = {
 			"Keyboard Input",
 			"  Press W : Jump                Press ESC : Back to Main Menu",
@@ -81,7 +120,10 @@ public class HowToPlayBox extends StackPane {
 			"Remind you that .. ","You need to collect all the notebooks!",
 			"                     As you know what to do   .. Ajarn Run !!"
 	};
-	
+	/**
+	 * instructions public static getter
+	 * @return instructions
+	 */
 	public static String[] getInstructions() {
 		return instructions;
 	}

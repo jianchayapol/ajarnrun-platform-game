b629d683a9c7afbd1fba7ce6a846c8dbaf15a378
Martin <6330080621@student.chula.ac.th>
2021-12-16 20:40:42 +0700

    ending level text box javadoc

--- src/gui/element/EndingLevelTextBox.java
+++ src/gui/element/EndingLevelTextBox.java
@@ -1,37 +1,47 @@
 package gui.element;
 
-import gui.button.ImageButton;
-import gui.button.ImageButtonType;
-import javafx.event.EventHandler;
-import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.Cursor;
 import javafx.scene.control.Label;
-import javafx.scene.effect.DropShadow;
-import javafx.scene.image.Image;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.CornerRadii;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
-import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Rectangle;
 import sharedObject.FontLoader;
 import sharedObject.FontType;
-import sharedObject.RenderableHolder;
 
+/**
+ * EndingLevelTextBox is a JavaFx StackPane that will be shown when a level is ended whether you die, your time is up, you fail a mission, or you complete a level.
+ * This class is just a StackPane that has a transparent background with a text shown at the center of the screen.
+ * @author Jian
+ *
+ */
 public class EndingLevelTextBox extends StackPane {
-
+	/**
+	 * bgRec is a backgroundRectangle, this is the background for this class, and it'll fit up the screen width and height.
+	 */
 	private static Rectangle bgRec;
+	/**
+	 * textLabel is a JavaFx Label object.
+	 * This is the text that is shown when a level is ended.
+	 */
 	private static Label textLabel;
+	/**
+	 * textSize is an integer used to set textLabel's size
+	 */
 	private int textSize;
+	/**
+	 * Screen's height
+	 */
 	private static final int HEIGHT = 600;
+	/**
+	 * Screen's width
+	 */
 	private static final int WIDTH = 800;
-	
+	/**
+	 * The constructor call setUpBackground method and create an empty String called "textToWrite".
+	 * Then the constructor use switch case with 4 cases to set textToWrite to a specific text.
+	 * Lastly, the constructor call method setUpTextLabel and add background and textLabel to the object itself.
+	 * @param text
+	 */
 	public EndingLevelTextBox(String text) {
 		
 		setUpBackground();
@@ -58,13 +68,20 @@ public class EndingLevelTextBox extends StackPane {
 		setUpTextLabel(textToWrite);
 		this.getChildren().addAll(bgRec,textLabel);
 	}
-	
+	/**
+	 * Initialize bgRec as a Rectangle object which is fitted to the screen.
+	 * Then set the background's opacity to 0.3
+	 */
 	private void setUpBackground() {
 		bgRec = new Rectangle(WIDTH, HEIGHT);
 		bgRec.setFill(Color.BLACK);
 		bgRec.setOpacity(0.3);
 	}
-
+	/**
+	 * Initialize textLabel with String textToWrite (which is the given parameter).
+	 * Then set some styles and setAlignment to Center.
+	 * @param textToWrite
+	 */
 	private void setUpTextLabel(String textToWrite) {
 		textLabel = new Label(textToWrite);
 		textLabel.setTextFill(Color.WHITESMOKE);
